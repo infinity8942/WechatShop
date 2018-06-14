a000a8490efa1779d2409d48e5de6c00e4a08783
ljlun <[lijialun@bjtiba.com]>
2018-06-14 10:51:49 +0800

    增加相册选择

--- app/src/main/java/com/qiushi/wechatshop/ui/manage/ManagerGoodsAdapter.kt
+++ app/src/main/java/com/qiushi/wechatshop/ui/manage/ManagerGoodsAdapter.kt
@@ -5,13 +5,17 @@ import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 import com.qiushi.wechatshop.R
 import com.qiushi.wechatshop.model.ShopOrder
+import com.qiushi.wechatshop.util.DensityUtils
 import com.qiushi.wechatshop.util.ImageHelper
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 class ManagerGoodsAdapter(data: List<ShopOrder>) : BaseQuickAdapter<ShopOrder, BaseViewHolder>(R.layout.manager_item_orther, data) {
     override fun convert(helper: BaseViewHolder?, item: ShopOrder?) {
         val view = helper?.getView<ImageView>(R.id.iv_shop)
-        ImageHelper.loadImage1(mContext, view!!, item?.cover!!, 93, 94, 10f)
-        helper.addOnClickListener(R.id.iv_more)
+
+        ImageHelper.loadImageWithCorner(mContext, view!!, item?.cover!!, 93, 94,
+                RoundedCornersTransformation(DensityUtils.dp2px(10.toFloat()), 0, RoundedCornersTransformation.CornerType.LEFT))
+        helper!!.addOnClickListener(R.id.iv_more)
                 .addOnClickListener(R.id.tv_zd)
                 .addOnClickListener(R.id.tv_delete)
                 .addOnClickListener(R.id.tv_xj)
