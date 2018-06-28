b40c0642ace7a1eec7decaae0312771693086235
ljlun <[lijialun@bjtiba.com]>
2018-06-28 16:18:30 +0800

    待办事项icon

--- app/src/main/java/com/qiushi/wechatshop/model/Shop.kt
+++ app/src/main/java/com/qiushi/wechatshop/model/Shop.kt
@@ -10,7 +10,7 @@ import java.io.Serializable
  */
 data class Shop(var id: Long, var name: String, var des: String, var logo: String, var cover: String, var is_owner: Boolean, var goods: ArrayList<Goods>
                 , var cash_flow: Float, var cash_forzen: Float, var cash_all: Float
-                , var msg_count: Int, var menu_list: ArrayList<Function>,var is_boss:String) : Serializable, IDraggable {
+                , var menu_list: ArrayList<Function>,var is_boss:String) : Serializable, IDraggable {
 
     override fun isDraggable(): Boolean {
         return !is_owner
