57900c149291a570a5fc73f7509cc4ef41677431
jiangjx <jiangjianxiong@bjtiba.com>
2018-06-29 14:45:06 +0800

    增加微信、支付宝调用

--- app/src/main/java/com/qiushi/wechatshop/util/web/WebActivity.java
+++ app/src/main/java/com/qiushi/wechatshop/util/web/WebActivity.java
@@ -2,6 +2,7 @@ package com.qiushi.wechatshop.util.web;
 
 import android.annotation.TargetApi;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -13,17 +14,35 @@ import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.TextView;
 
+import com.alipay.sdk.app.PayTask;
 import com.orhanobut.logger.Logger;
 import com.qiushi.wechatshop.R;
 import com.qiushi.wechatshop.model.PhoneInfo;
+import com.qiushi.wechatshop.net.BaseResponse;
+import com.qiushi.wechatshop.net.RetrofitManager;
+import com.qiushi.wechatshop.net.exception.Error;
+import com.qiushi.wechatshop.rx.BaseObserver;
+import com.qiushi.wechatshop.ui.pay.PayResultActivity;
+import com.qiushi.wechatshop.util.ToastUtils;
 import com.qiushi.wechatshop.util.Utils;
+import com.qiushi.wechatshop.wxapi.WXPayEntryActivity;
+import com.tencent.mm.opensdk.modelpay.PayReq;
 import com.tencent.sonic.sdk.SonicConfig;
 import com.tencent.sonic.sdk.SonicEngine;
 import com.tencent.sonic.sdk.SonicSession;
 import com.tencent.sonic.sdk.SonicSessionConfig;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Map;
 
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
+import io.reactivex.observers.DisposableObserver;
+import io.reactivex.schedulers.Schedulers;
 import me.imid.swipebacklayout.lib.app.SwipeBackActivity;
 
 /**
@@ -107,7 +126,11 @@ public class WebActivity extends SwipeBackActivity {
                     //step 6: Call sessionClient.requestResource when host allow the application
                     // to return the local data .
                     Logger.e("shouldInterceptRequest = " + url);
-                    return (WebResourceResponse) sonicSession.getSessionClient().requestResource(url);
+                    if (!url.isEmpty() && url.startsWith("local")) {
+                        getPayLocalData(url);
+                    } else {
+                        return (WebResourceResponse) sonicSession.getSessionClient().requestResource(url);
+                    }
                 }
                 return null;
             }
@@ -137,6 +160,130 @@ public class WebActivity extends SwipeBackActivity {
         // step 5: webview is ready now, just tell session client to bind
         sonicSessionClient.bindWebView(webView);
         sonicSessionClient.clientReady();
+    }
+
+    /**
+     * 获取支付订单号
+     */
+    private void getPayLocalData(String url) {
+        Uri uri = Uri.parse(url);
+        String numbers = uri.getQueryParameter("numbers");
+        if (!TextUtils.isEmpty(numbers)) {
+            String pay_type = uri.getQueryParameter("pay_type");
+            if (!TextUtils.isEmpty(pay_type)) {
+                try {
+                    int type = Integer.parseInt(pay_type);
+                    switch (type) {
+                        case 1://微信
+                            getWechatPay(numbers);
+                            break;
+                        case 2://支付宝
+                            getAliPay(numbers);
+                            break;
+                    }
+                } catch (NumberFormatException e) {
+                    Logger.e("getPayLocalData  Wrong pay_type");
+                }
+            } else {
+                ToastUtils.showError("支付信息出错，请重新下单");
+            }
+        } else {
+            ToastUtils.showError("支付信息出错，请重新下单");
+        }
+    }
+
+    /**
+     * 微信
+     */
+    private void getWechatPay(String numbers) {
+        Disposable disposable = RetrofitManager.INSTANCE.getService().getWechatPay(numbers)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new BaseObserver<PayReq>() {
+                    @Override
+                    protected void onHandleSuccess(PayReq payReq) {
+                        if (null != payReq) {
+                            if (!WXPayEntryActivity.pay(getApplicationContext(), payReq)) {
+                                ToastUtils.showWarning("未安装微信客户端");
+                            }
+                        }
+                    }
+
+                    @Override
+                    protected void onHandleError(@NotNull Error e) {
+                        ToastUtils.showError(e.getMsg());
+                    }
+                });
+    }
+
+    /**
+     * 支付宝
+     */
+    private void getAliPay(String numbers) {
+        Disposable disposable = RetrofitManager.INSTANCE.getService().getAliPay(numbers)
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.io())
+                .flatMap(new Function<BaseResponse<String>, ObservableSource<Map<String, String>>>() {
+                    @Override
+                    public ObservableSource<Map<String, String>> apply(BaseResponse<String> s) {
+                        PayTask pay = new PayTask(WebActivity.this);
+                        return Observable.just(pay.payV2(s.getData(), true));
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(new DisposableObserver<Map<String, String>>() {
+                    @Override
+                    public void onNext(Map<String, String> map) {
+                        Logger.d("aliResp status = " + map.get("resultStatus"));
+                        Logger.d("aliResp result = " + map.get("result"));
+                        Logger.d("aliResp memo = " + map.get("memo"));
+
+                        switch (map.get("resultStatus")) {
+                            case "9000":
+                                startActivity(new Intent(WebActivity.this, PayResultActivity.class));
+                                //PayResultActivity.SUCCESS
+                                break;
+                            case "8000":
+                            case "6004":
+                                ToastUtils.showError("支付状态异常");
+                                startActivity(new Intent(WebActivity.this, PayResultActivity.class));
+                                //PayResultActivity.FAILED
+                                break;
+                            case "4000":
+                                startActivity(new Intent(WebActivity.this, PayResultActivity.class));
+                                //PayResultActivity.FAILED
+                                break;
+                            case "5000":
+                                ToastUtils.showWarning("重复请求");
+                                break;
+                            case "6001":
+                                ToastUtils.showWarning("您已取消支付");
+                                break;
+                            case "6002":
+                                ToastUtils.showError("网络错误");
+                                break;
+                            default:
+                                ToastUtils.showError("支付错误");
+                                startActivity(new Intent(WebActivity.this, PayResultActivity.class));
+                                //PayResultActivity.FAILED
+                                break;
+                        }
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        if (e != null) {
+                            e.printStackTrace();
+                            ToastUtils.showError(e.getMessage());
+                        } else {
+                            ToastUtils.showError("获取订单信息失败，请重试");
+                        }
+                    }
+
+                    @Override
+                    public void onComplete() {
+                    }
+                });
     }
 
     @Override
